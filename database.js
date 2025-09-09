d11fd4e9a066506ba4c3b56354eea0092b1efc28
lidia <lidi4silva0@gmail.com>
2025-09-09 16:44:31 -0300

    feat: servidor express funcionando, database comentado para evitar crash

---
+++ database.js
@@ -0,0 +1,22 @@
+const { FULL } = require("sqlite3");
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./tarefas.db");
+
+db.serialize(() => {
+    db.run (`
+        CREATE TABLE IF NOT EXISTS tarefas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            dataCriacao TETXT NOT NULL
+    )
+    `, (err) => {
+        if (err) {
+        console.log("Erro ao criar tabela:", err.message);
+        } else {
+            console.log("Tabela 'tarefas' pronta!");
+        }
+    });
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -2,6 +2,36 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const db = require("./database.js");
+
+function inserirTarefa(nome) {
+    const data = new Date().toISOString();
+    db.run(
+        `INSERT INTO tarefas (nome, dataCriacao) VALUES (?, ?),`
+        [nome, data],
+        function(err) {
+            if (err) {
+                console.log("Erro ao inserir tarefa:", err.message);
+            } else {
+                console.log(`Tarefa inserida com id ${this.lastID}`);
+            }
+        }
+    );
+}
+
+function listarTarefas() {
+    db.all("SELECT * FROM tarefas", (err, rows) => {
+        if (err) {
+            console.log("Erro ao listar tarefas:", err.message);
+        } else {
+            console.log("Tarefas cadastradas:", rows);
+        }
+    });
+}
+
+// inserirTarefa("Minha primeira tarefa");
+// listarTarefas();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
