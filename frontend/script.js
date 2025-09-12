f415c4095cbf1a03dd8d9152ce5dc58661b03a26
lidia <lidi4silva0@gmail.com>
2025-09-12 16:12:58 -0300

    fix: reestruturação das pastas (backend separado do front)

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+const API_URL = "http://localhost:3000/tarefas";
+
+const but = document.getElementById("butAdicionar");
+const input = document.getElementById("tarefasInput");
+const lista = document.getElementById("listaTarefas");
+
+async function carregarTarefas () {
+    
+}
