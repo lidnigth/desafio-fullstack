91cc91ea09a53b43574a1fb11e53ef9b4d79f497
lidia <lidi4silva0@gmail.com>
2025-10-02 14:30:55 -0300

    Revert "feat: rota PUT"
    This reverts commit 8e8c4f5654886311bba25e323d9400b0fa4068e8.

--- backend/routes/tarefaRoutes.js
+++ backend/routes/tarefaRoutes.js
@@ -5,6 +5,5 @@ const tarefaController = require("../controllers/tarefaController");
 router.get("/", tarefaController.getTarefas);
 router.post("/", tarefaController.postTarefa);
 router.delete("/:id", tarefaController.deleteTarefa);
-router.put("/:id", tarefaController.putTarefa);
 
 module.exports = router;
--- backend/services/tarefaService.js
+++ backend/services/tarefaService.js
@@ -9,39 +9,15 @@ function inserirTarefa(nome, dataCriacao, callback) {
     "INSERT INTO tarefas (nome, dataCriacao) VALUES (?, ?)",
     [nome, dataCriacao],
     function (err) {
-      if (err) return callback(err);
-      
-      callback(null, { id: this.lastID, nome, dataCriacao });
+      callback(err, { id: this.lastID, nome, dataCriacao });
     }
   );
 }
 
 function deletarTarefa(id, callback) {
-  db.run("DELETE FROM tarefas WHERE id = ?",
-    [id],
-    function (err) {
-    if (err) return callback(err);
-    if (this.changes === 0) {
-      return callback(new Error("Tarefa não encontrada"));
-    }
-    callback(null, { id });
+  db.run("DELETE FROM tarefas WHERE id = ?", [id], function (err) {
+    callback(err, this.changes);
   });
 }
 
-function atualizarTarefa(id, nome, callback) {
-  db.run(
-    "UPDATE tarefas SET nome = ? WHERE id = ?",
-    [nome, id],
-    function (err) {
-      if (err) return callback(err)
-
-      if (this.changes === 0) {
-        return callback(new Error("Tarefa não encontrada"));
-      }
-
-      callback(null, { id, nome });
-    }
-  );
-}
-
-module.exports = { listarTarefas, inserirTarefa, deletarTarefa, atualizarTarefa };
+module.exports = { listarTarefas, inserirTarefa, deletarTarefa };
